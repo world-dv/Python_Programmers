50ab0e0ee7165d8d5a07c045b612e636d6406a6a
Hyojeong <79013520+world-dv@users.noreply.github.com>
2023-11-25 04:13:13 +0900

    Update DataAnalysis.py
    2중 for문에서 for문 하나로 줄임

--- PCCE_기출문제/DataAnalysis.py
+++ PCCE_기출문제/DataAnalysis.py
@@ -1,5 +1,8 @@
 def solution(data, ext, val_ext, sort_by):
     arr = {"code":0, "date":1, "maximum":2, "remain":3}
-    answer = [[i for i in data[j]] for j in range(len(data)) if data[j][arr[ext]] < val_ext]
+    answer = []
+    for i in range(len(data)):
+        if data[i][arr[ext]] < val_ext:
+            answer.append(data[i])
     answer.sort(key = lambda x : x[arr[sort_by]])
     return answer
