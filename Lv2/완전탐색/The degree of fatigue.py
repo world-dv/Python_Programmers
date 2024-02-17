5652963a22ce6315f1a13f60fb34bd1c2eee4777
Hyojeong <79013520+world-dv@users.noreply.github.com>
2024-02-17 17:03:49 +0900

    Update The degree of fatigue.py

--- Lv2/완전탐색/The degree of fatigue.py
+++ Lv2/완전탐색/The degree of fatigue.py
@@ -3,10 +3,10 @@ def solution(k, dungeons):
     visited = [0 for _ in range(len(dungeons))]
     
     def dfs(hp, visited):
-        if sum(visited) not in answer:
-            answer.append(sum(visited))
         if hp <= 0:
             return
+        if sum(visited) not in answer:
+            answer.append(sum(visited))
         for i, j in enumerate(dungeons):
             if not visited[i] and hp >= j[0]:
                 visited[i] = 1
